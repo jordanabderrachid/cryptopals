3c0b8158ea4e401aadc5f1da3fcc9515b1700e35
Jordan Abderrachid <jabderrachid@gmail.com>
2022-10-08 15:29:27 +0100

    challenge 1.2

--- main.go
+++ main.go
@@ -17,6 +17,29 @@ func hexToBase64(in string) string {
 	return base64.StdEncoding.EncodeToString(bytes)
 }
 
+func xor(lhs, rhs string) string {
+	lhsBytes, err := hex.DecodeString(lhs)
+	panicIfErr(err)
+
+	rhsBytes, err := hex.DecodeString(rhs)
+	panicIfErr(err)
+
+	return hex.EncodeToString(xorBytes(lhsBytes, rhsBytes))
+}
+
+func xorBytes(lhs, rhs []byte) []byte {
+	if len(lhs) != len(rhs) {
+		panic("tried to xor []byte of unequal length")
+	}
+
+	size := len(rhs)
+	res := make([]byte, size)
+	for i := 0; i < size; i++ {
+		res[i] = lhs[i] ^ rhs[i]
+	}
+	return res
+}
+
 func panicIfErr(err error) {
 	if err != nil {
 		panic(err)
--- main_test.go
+++ main_test.go
@@ -12,3 +12,12 @@ func TestHexToBase64(t *testing.T) {
 
 	assert.Equal(t, expectedOutput, output)
 }
+
+func TestXOR(t *testing.T) {
+	lhs, rhs := "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"
+	expectedOutput := "746865206b696420646f6e277420706c6179"
+
+	output := xor(lhs, rhs)
+
+	assert.Equal(t, expectedOutput, output)
+}
